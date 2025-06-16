98e94dc8608a7cdf3dd4fdf70220ad27247f1a18
felix <felixxplore08@gmail.com>
2025-06-16 19:00:33 +0530

    MT-001: Initialize Spring boot project for meratodo-backend

---
+++ src/main/java/com/meratodo/backend/MeratodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.meratodo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeratodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeratodoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meratodo/backend/MeratodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meratodo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeratodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
